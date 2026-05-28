1340b3fa1ca37947a59d1cba0b4633e0a1ccfbcf
NoctusLynx <igarayuahernandez28@floridapoly.edu>
2026-05-28 18:03:02 +0000

    Added various functions, requires testing, requires check algorithm and QOL changes.

--- InMediaChess.c
+++ InMediaChess.c
@@ -1,6 +1,9 @@
+// References
+#pragma region
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <bool.h>
 
 void PrintGreeting(void);    
 void GenerateBoard(void);
@@ -8,8 +11,18 @@ void PrintBoard(char whiteBoard[8][8], char blackBoard[8][8]);
 void PrintLine(void);
 void InitializeBoards(char WhiteBoard[8][8], char BlackBoard[8][8]);
 char ToLowerCase(char letter);
+bool SpaceEmpty(char WhiteBoard[8][8], char blackBoard[8][8]);
 void PlacePawns(char whiteBoard[8][8], char blackBoard[8][8], int bonusIterations[4]);
+void PlaceRooks(char whiteBoard[8][8], char blackBoard[8][8], int extraRooks);
+void PlaceBishops(char whiteBoard[8][8], char blackBoard[8][8], int extraBishops);
+void PlaceKnights(char whiteBoard[8][8], char blackBoard[8][8], int extraKnights);
+void PlaceQueens(char whiteBoard[8][8], char blackBoard[8][8], int extraQueens);
+void PlaceKing(char whiteBoard[8][8], char blackBoard[8][8]);
+void Check(char whiteBoard[8][8], char blackBoard[8][8], int posX, int posY);
+#pragma endregion
 
+// Main and Ultilities
+#pragma region
 int main(void)
 {
     PrintGreeting();
@@ -48,6 +61,35 @@ void PrintGreeting(void)
     printf("\n\n");
 }
 
+void PrintLine(void)
+{
+    printf("  -------------------\n");
+}
+
+char ToLowerCase(char letter)
+{
+    switch(letter)
+    {
+        case 'P':
+        return 'p';
+
+        case 'R':
+        return 'r';
+
+        case 'B':
+        return 'b';
+
+        case 'N':
+        return 'n';
+
+        case 'K':
+        return 'k';
+
+        case 'Q':
+        return 'q';
+    }
+}
+
 void GenerateBoard(void)
 {
     char whiteBoard[8][8];
@@ -57,7 +99,7 @@ void GenerateBoard(void)
 
     /*
     Generation notes:
-    1. No more than 16 pieces for each
+    1. No more than 16 pieces for each player
     2. No more than 8 pawns
     3. Pawn has 20% chance, each, to not exist, 5% chance to be promoted to a different piece
     4. All other pieces (except King) have a 10% chance to not exist
@@ -66,15 +108,22 @@ void GenerateBoard(void)
     // Place Pawns (8 ct, not on first or final row)
     PlacePawns(whiteBoard, blackBoard, bonusIterations);
 
-    // Place Queens (1 ct, anywhere)
-
-    // Place Bishops (2ct: one on Dark Square, one on Light square)
+    // Note: Bonus iterations are in order as listed here
 
     // Place Rooks (2 ct, place anywhere)
+    PlaceRooks(whiteBoard, blackBoard, bonusIterations[0]);
+
+    // Place Bishops (2ct: one on Dark Square, one on Light square)
+    PlaceBishops(whiteBoard, blackBoard, bonusIterations[1]);
 
     // Place Knights (2 ct, place anywhere)
+    PlaceKnight(whiteBoard, blackBoard, bonusIterations[2]);
+
+    // Place Queens (1 ct, anywhere)
+    PlaceQueens(whiteBoard, blackBoard, bonusIterations[3]);
 
     // Place Kings (1 ct, re-run if check)
+    PlaceKing()
 
     PrintBoard(whiteBoard, blackBoard);
     
@@ -93,6 +142,15 @@ void InitializeBoards(char whiteBoard[8][8], char blackBoard[8][8])
     }
 }
 
+bool SpaceEmpty(char whiteBoard[8][8], char blackBoard[8][8])
+{
+    
+}
+#pragma endregion
+
+
+
+#pragma region
 void PrintBoard(char whiteBoard[8][8], char blackBoard[8][8])
 {
     PrintLine();
@@ -106,7 +164,7 @@ void PrintBoard(char whiteBoard[8][8], char blackBoard[8][8])
         {
             if(whiteBoard[i][j] != '.' && blackBoard[i][j] != '.')
             {
-                printf("X ", i, j);
+                printf("X ");
             }
             
             else if(whiteBoard[i][j] == '.' && blackBoard[i][j] == '.')
@@ -170,7 +228,8 @@ void PlacePawns(char whiteBoard[8][8], char blackBoard[8][8], int bonusIteration
             }
         }
         
-        else if(rng < 2)
+        // 20% chance to not exist
+        else if(rng < 3)
         {
             continue;
         }
@@ -181,11 +240,11 @@ void PlacePawns(char whiteBoard[8][8], char blackBoard[8][8], int bonusIteration
             rng = rand() % 6 + 1;
             
             // Checks if that space is available
-            if(whiteBoard[rng][i] != '.' && blackBoard != '.')
+            if(whiteBoard[rng][i] != '.' && blackBoard[7-rng][7-i] != '.')
             {
                 i--;
 
-                printf("Duplicate found at (%d, %d)!\n\n", rng, i);
+                //printf("Duplicate found at (%d, %d)!\n\n", rng, i);
 
                 continue;
             }    
@@ -197,35 +256,178 @@ void PlacePawns(char whiteBoard[8][8], char blackBoard[8][8], int bonusIteration
     }
 }
 
-void PrintLine(void)
+void PlaceRooks(char whiteBoard[8][8], char blackBoard[8][8], int extraRooks)
 {
-    printf("  -------------------\n");
-}
-
-char ToLowerCase(char letter)
-{
-    switch(letter)
+    for(int i = 0; i < 2 + extraRooks; i ++)
     {
-        case 'P':
-        return 'p';
-        
-        case 'K':
-        return 'k';
+        int coin = rand() % 10;
 
-        case 'Q':
-        return 'q';
+        // 10% chance to not exist
+        if(coin < 1)
+        {
+            continue;
+        }
+            
 
-        case 'B':
-        return 'b';
+        int rng1 = rand() % 8;
+        int rng2 = rand() % 8;
 
-        case 'N':
-        return 'n';
+        // If the space is occupied, re-roll position
+        if(whiteBoard[rng1][rng2] != '.' || blackBoard[7 - rng1][7 - rng2] != '.')
+        {
+            i--;
+            continue;
+        }
 
-        case 'R':
-        return 'r';
-
-        default:
-        printf("ERROR USING \"ToLowerCase\" FUNCTION!");
-        return '\0';
+        // If space is not occupied, then place the queen there
+        else
+        {
+            whiteBoard[rng1][rng2] = 'R';
+            blackBoard[7 - rng1][7 - rng2] = 'R';
+        }
     }
 }
+
+void PlaceBishops(char whiteBoard[8][8], char blackBoard[8][8], int extraBishops)
+{
+    for(int i = 0; i < 2 + extraBishops; i ++)
+    {
+        int coin = rand() % 10;
+
+        // 10% chance to not exist
+        if(coin < 1)
+        {
+            continue;
+        }
+            
+        // 50% chance to be a light or dark square bishop
+        coin = rand() % 2;
+
+        // Guarantees that the first two bishops are on opposite colored squares for fairness
+        if(i == 0)
+        {
+            coin = 0;
+        }
+        else if(i == 1)
+        {
+            coin = 1;
+        }
+            
+
+        int rng1 = 2 * (rand() % 4);
+        int rng2 = 2 * (rand() % 4);
+
+        // If coin is even, then both rng values are even; if coid is odd, both rng values are odd
+        if(coin == 1)
+        {
+            rng1++;
+            rng2++;
+        }
+
+        // If the space is occupied, re-roll position
+        if(whiteBoard[rng1][rng2] != '.' || blackBoard[7 - rng1][7 - rng2] != '.')
+        {
+            i--;
+            continue;
+        }
+
+        // If space is not occupied, then place the queen there
+        else
+        {
+            whiteBoard[rng1][rng2] = 'B';
+            blackBoard[7 - rng1][7 - rng2] = 'B';
+        }
+    }
+}
+
+void PlaceKnights(char whiteBoard[8][8], char blackBoard[8][8], int extraKnights)
+{
+    for(int i = 0; i < 2 + extraKnights; i ++)
+    {
+        int coin = rand() % 10;
+        
+        if(coin < 1)
+        {
+            continue;
+        }
+
+        int rng1 = rand() % 8;
+        int rng2 = rand() % 8;
+
+        // If the space is occupied, re-roll position
+        if(whiteBoard[rng1][rng2] != '.' || blackBoard[7 - rng1][7 - rng2] != '.')
+        {
+            i--;
+            continue;
+        }
+
+        // If space is not occupied, then place the queen there
+        else
+        {
+            whiteBoard[rng1][rng2] = 'K';
+            blackBoard[7 - rng1][7 - rng2] = 'K';
+        }
+    }
+}
+
+void PlaceQueens(char whiteBoard[8][8], char blackBoard[8][8], int extraQueens)
+{
+    for(int i = 0; i < 1 + extraQueens; i ++)
+    {
+        int rng1 = rand() % 8;
+        int rng2 = rand() % 8;
+
+        // If the space is occupied, re-roll position
+        if(whiteBoard[rng1][rng2] != '.' || blackBoard[7 - rng1][7 - rng2] != '.')
+        {
+            i--;
+            continue;
+        }
+
+        // If space is not occupied, then place the queen there
+        else
+        {
+            whiteBoard[rng1][rng2] = 'Q';
+            blackBoard[7 - rng1][7 - rng2] = 'Q';
+        }
+    }
+}
+
+void PlaceKing(char whiteBoard[8][8], char blackBoard[8][8])
+{
+    int rng1 = rand() % 10;
+    int rng2 = rand() % 10;
+
+    for(int i = 0; i < 1; i++)
+    {
+        if(whiteBoard[rng1][rng2] != '.' || blackBoard[7 - rng1][7 - rng2] != '.' || Check(whiteBoard, blackBoard, rng1, rng2))
+        {
+            i--;
+            continue;
+        }
+        else
+        {
+            whiteBoard[rng1][rng2] = 'K';
+            blackBoard[7 - rng1][7 - rng2] = 'K';
+            break;
+        }
+
+    }
+}
+
+int Check(char whiteBoard[8][8], char blackBoard[8][8], int posX, int posY)
+{
+    // return 0 = false
+    // return 1 = true
+
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            
+        }
+    }
+    
+    return 0;
+}
+#pragma endregion
